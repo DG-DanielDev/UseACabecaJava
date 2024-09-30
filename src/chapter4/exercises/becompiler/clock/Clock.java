d12e5eadcc208cf0342ac07b5fed323729970194
DanielDG <danielramososielrodrigues@gmail.com>
2024-09-30 13:41:47 -0300

    Add complete chapter 4
    Signed-off-by: DanielDG <danielramososielrodrigues@gmail.com>

---
+++ src/chapter4/exercises/becompiler/clock/Clock.java
@@ -0,0 +1,11 @@
+package chapter4.exercises.becompiler.clock;
+
+public class Clock {
+  String time;
+   void setTime(String t) {
+     time = t;
+   }
+   String getTime() {
+     return time;
+   }
+}
